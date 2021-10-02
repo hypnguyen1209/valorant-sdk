ac420ed2ef68f932066489f614ba61e3753d3744
Hip <hypnguyen1209@gmail.com>
2021-10-02 19:01:10 +0700

    update get infomation store

--- src/getPlayerStore.js
+++ src/getPlayerStore.js
@@ -6,5 +6,54 @@ module.exports = async detail => {
             'X-Riot-Entitlements-JWT': detail.entitlements_token
         }
     })
-    return resp.data
+    const { DataAssetID } = resp.data.FeaturedBundle.Bundle
+    const { SingleItemOffers } = resp.data.SkinsPanelLayout
+    const { BundleRemainingDurationInSeconds } = resp.data.FeaturedBundle
+    const { SingleItemOffersRemainingDurationInSeconds } = resp.data.SkinsPanelLayout
+    const getInfo = await Promise.all([
+        axios.get(`https://valorant-api.com/v1/bundles/${DataAssetID}`),
+        ...SingleItemOffers.map(async item => {
+            return axios.get(`https://valorant-api.com/v1/weapons/skinlevels/${item}`)
+        })
+    ])
+    const store = {
+        bundle: {
+            ...getInfo[0].data,
+            timeRemaining: BundleRemainingDurationInSeconds
+        },
+        itemOffers: {
+            items: [
+                ...getInfo.filter((_, i) => i > 0 && i < 5).map(e => e.data)
+            ],
+            timeRemaining: SingleItemOffersRemainingDurationInSeconds
+        }
+    }
+    if (resp.data.BonusStore.BonusStoreOffers.length === 6) {
+        const { BonusStoreRemainingDurationInSeconds } = resp.data.BonusStore
+        const { BonusStoreOffers } = resp.data.BonusStore
+        const getOffersId = BonusStoreOffers.map(item => item.Offer.OfferID)
+        const getNightMarket = await Promise.all([
+            ...getOffersId.map(async item => {
+                return axios.get(`https://valorant-api.com/v1/weapons/skinlevels/${item}`)
+            })
+        ])
+        const nightMarket = getNightMarket.map((item, i) => {
+            return {
+                ...item.data,
+                discountPercent: BonusStoreOffers[i].DiscountPercent,
+                discountCosts: BonusStoreOffers[i].DiscountCosts['85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741'],
+                cost: BonusStoreOffers[i].Offer.Cost['85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741']
+            }
+        })
+        return {
+            store,
+            nightMarket: {
+                data: [
+                    ...nightMarket
+                ],
+                timeRemaining: BonusStoreRemainingDurationInSeconds
+            }
+        }
+    }
+    return { store }
 }
--- test/index.js
+++ test/index.js
@@ -2,7 +2,13 @@ const { getPlayerStore, getMatchHistory, ValorantClient, getInventory, getWallet
 const valorantClient = new ValorantClient()
 const { username, password } = require('../config.json')
 const main = async detail => {
-    let wallet = await getWallet(detail)
-    console.log(wallet)
+    let wallet = await getPlayerStore(detail)
+    console.log(JSON.stringify(wallet))
 }
-valorantClient.login(username, password).then(main)
+const errHandlers = async detail => {
+    console.log(detail.message)
+}
+valorantClient
+    .login(username, password)
+    .then(main)
+    .catch(errHandlers)
